a812bec23add124c4ac4ad3f8af45715b2e55f54
georgekazanba <90256349+georgekazanba@users.noreply.github.com>
2021-09-14 09:59:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<!Doctype>
+<html>
+<head>
+	<!--Ссылка на файл со свойствами сайта-->
+	<link rel="stylesheet" href="master.css">
+    <title>MyGiro: гироскутеры, электросамокаты, сигвеи</title>
+</head>
+<body>
+	<header>
+		<!--Логотип компании-->
+		<div class="logo">
+			<img src="img/logo.png" class="logo-img" height=140px>
+		</div>
+		<!--Номер телефона компании-->		
+		<div class="logo2">
+			<img src="img/call.png" class="logo-img2" height=55px width=55px>
+			<span class="logo-title">+7 351 111 22 33</span>
+		</div>
+	</header>
+	<!--Слайдер-->
+	<div class="carousel-wrapper">
+		<span id="item-1"></span>
+		<span id="item-2"></span>
+		<span id="item-3"></span>
+		<span id="item-4"></span>
+		<!-- 1й элемент слайдера -->
+		<div class="carousel-item item-1">
+		  <center><p>Мы обслуживаем одинаково качественно как своих гарантийных клиентов, так и сторонние заказы.</p></center>
+		  <a class="arrow arrow-prev" href="#item-4"></a>
+		  <a class="arrow arrow-next" href="#item-2"></a>
+		</div>
+		<!-- 2й элемент слайдера -->
+		<div class="carousel-item item-2">
+		  <center><p>Если деталь сломалась — в нашем центре ее не будут ремонтировать, а сразу заменят на новую.</p></center>
+		  <a class="arrow arrow-prev" href="#item-1"></a>
+		  <a class="arrow arrow-next" href="#item-3"></a>
+		</div>
+		<!-- 3й элемент слайдера -->
+		<div class="carousel-item item-3">
+		  <center><p>Мы работаем с крупной сетью сервисных центров, ее филиалы находятся в 70 городах России.</p></center>
+		  <a class="arrow arrow-prev" href="#item-2"></a>
+		  <a class="arrow arrow-next" href="#item-4"></a>
+		</div>
+		<!-- 3й элемент слайдера -->
+		<div class="carousel-item item-4">
+		  <center><p>Гарантия действует в течении 3 лет!</p></center>
+		  <a class="arrow arrow-prev" href="#item-3"></a>
+		  <a class="arrow arrow-next" href="#item-1"></a>
+		</div>
+	</div>
+	<div class="wrapper">
+		<img src="img/elsam.jpg" width="425px" height="425px" class="content">
+		<span class="p">Электросамокат - современный вариант простого самоката. Для того чтобы на нем кататься, нужно всего один раз оттолкнуться ногой от земли, обеспечив стартовое ускорение. Дальше устройство будет везти хозяина самостоятельно за счет встроенного электромотора. Если выбирать между моноколесом или электросамокатом, последний будет гораздо безопаснее в эксплуатации за счет наличия 2 колес (т.е. 2 точек опоры) и ручки для опоры руками.</span>
+	</div>
+	<div class="wrapper">
+		<img src="img/gyro.jpg" width="425px" height="425px" class="content">
+		<span class="p">Гироскутер представляет собой конструкцию из 2 колес, расположенных по бокам от наездника и соединенных поперечной планкой-корпусом. Внутри корпуса располагаются электродвигатели, аккумуляторные батареи и гироскопы, поддерживающие в горизонтальном положении площадку для ног. Гироскутеры никогда не оснащаются рукоятками для рук. Устройства из 2 колес, поперечной площадки и рулевого столба называются сигвеями и управляются манипуляциями руками. Особенность управления гироскутером состоит в том, что для движения вперед наезднику необходимо наклониться вперед. Для торможения или движения назад - наклониться назад. Для поворота нужно наклонять корпус в правую или левую сторону.</span>
+	</div>
+	<div class="wrapper">
+		<img src="img/seegway.jpg" width="425px" height="425px" class="content">
+		<span class="p">Сигвей - средство передвижения для одного человека, которое держит равновесие с помощью специальной автобалансирующей системы. Это своеобразный электрический скутер, самостоятельно удерживающий равновесие. Сигвеи бесшумны в работе, не загрязняют окружающую среду, работают на электрической энергии, накопленной батареей аккумуляторов.</span>
+	</div>
+	<center>
+		<p class="table1">
+			<table>
+				<tr>
+					<th>Фото</th>
+					<th>Название</th>
+					<th>Цена</th>
+					<th>Вид</th>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov1.jpg" width=200px height=200px></td>
+					<td><p>Smart Balance 6.5" Ver. 1</p></td>
+					<td><p>8990 р.</p></td>
+					<td><p>Гироскутер</p></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov2.jpg" width=200px height=200px></td>
+					<td><p></p></td>
+					<td><p></p></td>
+					<td><p></p></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov3.jpg" width=200px height=200px></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov4.jpg" width="20%" height=200px></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov5.jpg" width=200px height=200px></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov6.jpg" width=200px height=200px></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td align="center"><img src="img/tov7.jpg" width=200px height=200px></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+		</p>
+	</center>
+	<center>
+		<table class="consult">
+			<tr>
+				<th></th>
+				<th>Консультанты</th>
+				<th></th>
+			</tr>
+			<tr>
+				<td align="center"><img src="img/placeholder1.png" width="100px" height="100px"></td>
+				<td align="center"><img src="img/placeholder2.jpg" width="100px" height="100px"></td>
+				<td align="center"><img src="img/placeholder3.jpg" width="100px" height="100px"></td>
+			</tr>
+			<tr>
+				<td align="center"><p>Захаров Андрей Борисович</p></td>
+				<td align="center"><p>Пупкин Василий Васильевич</p></td>
+				<td align="center"><p>Иванов Иван Иванович</p></td>
+			</tr>
+			<tr>
+				<td width=300px align="center"><p>Менеджер-консультант My Gyro</p></td>
+				<td width=300px align="center"><p>Старший консультант My Gyro</p></td>
+				<td width=300px align="center"><p>Квалифицированный специалист сервисного центра My Gyro</p></td>
+			</tr>
+		</table>
+	</center>
+	<center><img src="img/personal.jpg" width=1920px height=428px></center>
+	<center><p class="per">Чтобы работать в наших магазинах нужно знать каждую модель, её плюсы и минусы и уметь ездить на ней. Да, это долго, и кто-то скажет, что неэффективно. Но так мы гарантированно получаем сотрудника, который разбирается в каждом товаре и сможет по-настоящему помочь покупателю, а не “впихнуть” ему, что-нибудь подороже.</p></center>
+    <center><p class="per">Наш магазин работает с 2016 года. Мы представлены в таких городах как Екатеринбург, Москва и Челябинск и многие другие. В среднем мы отправляем по 15-20 заказов в день в разные города и регионы по всей России. Мы имеем свои сервисные центры и собственных мастеров, благодаря которым, выполняем гарантийные работы качественно и в срок. Так же вы можете обратиться к нам с любой проблемой. Мастера выполнят диагностику и выполнят ремонт.</p></center>
+</body>
+</html>
